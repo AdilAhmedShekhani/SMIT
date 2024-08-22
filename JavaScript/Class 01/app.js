25ef46f62f39fd40aa14da61cd28252ea49756e3
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-08-22 08:57:38 +0500

    Alert/ Variable & Types / Math Expression

---
+++ JavaScript/Class 01/app.js
@@ -0,0 +1,45 @@
+alert("Hello Student");
+
+// Declaring a variable with the var keyword
+var age;
+// Assigning a value to a variable
+age = 25;
+// Declaring and assigning a value to a variable
+var name = "John";
+
+// JavaScript supports various data types, including numbers, strings, booleans, null, undefined, and objects.
+
+// Number
+var age = 25;
+
+// String
+var name = "John";
+
+// Boolean
+var isMarried = true;
+
+// Null
+var car = null;
+
+// Undefined
+var bike;
+
+// Object
+var person = { name: "John", age: 25 };
+
+// JavaScript supports various arithmetic operators, including addition (+), subtraction (-), multiplication (*), division (/), and modulus (%).
+
+// Addition
+var sum = 2 + 3;
+
+// Subtraction
+var difference = 5 - 2;
+
+// Multiplication
+var product = 2 * 3;
+
+// Division
+var quotient = 10 / 2;
+
+// Modulus
+var remainder = 10 % 3;
