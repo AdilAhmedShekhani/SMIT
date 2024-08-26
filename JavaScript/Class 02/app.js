3ee2379fe83d6891e1482c577f89d8082fa6aab6
AdilAhmedShekhani <adilaziz2009@live.com>
2024-08-26 15:36:07 +0500

    Comparison operators

--- JavaScript/Class 02/app.js
+++ JavaScript/Class 02/app.js
@@ -15,3 +15,28 @@ z -= 3
 
 //Comparison operators: ==, ===,!=, !==, etc.:
 
+// Equal operator
+var x = 5
+if (x == 5) {
+  console.log("x is equal to 5")
+}
+
+// Strict equal operator
+var y = "5"
+if (y === 5) {
+  console.log("y is equal to 5")
+} else {
+  console.log("y is not equal to 5")
+}
+
+// Not equal operator
+var z = 10
+if (z != 5) {
+  console.log("z is not equal to 5")
+}
+
+// Strict not equal operator
+var a = "5"
+if (a !== 5) {
+  console.log("a is not equal to 5")
+}
