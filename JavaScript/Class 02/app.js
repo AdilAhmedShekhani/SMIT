0d845abfb4f1328f2fbe8b827bc109f01a675d3c
AdilAhmedShekhani <adilaziz2009@live.com>
2024-08-26 15:35:07 +0500

    Assignment Operator

--- JavaScript/Class 02/app.js
+++ JavaScript/Class 02/app.js
@@ -1,2 +1,17 @@
 alert("Hello Student");
 
+//Assignment operators: =, +=, -=, etc.:
+
+// Equal operator
+var x = 5
+
+// Addition assignment operator
+var y = 10
+y += 5
+
+// Subtraction assignment operator
+var z = 10
+z -= 3
+
+//Comparison operators: ==, ===,!=, !==, etc.:
+
