af4951421593a73c45941867422f5ab2319371ff
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-09-03 09:09:35 +0500

    Conditional Statements (If / Else / Else IF)

--- JavaScript/Class 03/app.js
+++ JavaScript/Class 03/app.js
@@ -28,3 +28,93 @@ alert("Hello Students");
 // if (a !== 5) {
 //   console.log("a is not equal to 5")
 // }
+
+// Evaluating expressions and order of operations:
+// JavaScript follows the order of operations when evaluating expressions. Parentheses are used to specify the order of operations.
+
+// // Example expression
+// var result = (2 + 3) \* 4;
+
+// Conditional Statements:
+// If...else statements:
+// If...else statements are used to execute different code blocks based on a condition in JavaScript.
+
+// Example if... statement
+// if statements (true or false)
+
+// if (true) {
+//   alert("Good");
+// }
+
+// var city = prompt("The city of lights ?");
+// if (city === "karachi") {
+//   alert("Correct");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// if (gender === "Male") {
+//   alert("Welcome Sir");
+// }
+
+// if (gender === "Female") {
+//   alert("Welcome Maam");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// var message = "Please Enter Correct one";
+// if (gender === "Male") {
+//   message = "Welcome Sir";
+// }
+
+// if (gender === "Female") {
+//   message = "Welcome Maam";
+// }
+
+// if else and else if statements
+
+// var age = 22;
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+// var kuchLanahai = prompt("Kia mil raha hai ");
+
+// if (kuchLanahai === "Dahi") {
+//   console.log("le ana");
+// } else if (kuchLanahai === "Dhoodh") {
+//   console.log("Acha Le aao");
+// } else if (kuchLanahai === "Bread") {
+//   console.log("bhai le aao yar");
+// } else {
+//   console.log("Bhai kuch bhi le aao");
+// }
+
+// var language = prompt("Enter Your Language");
+
+// if (language === "javascript") {
+//   var city = prompt("Enter your city");
+//   if (city === "karachi") {
+//     var exp = prompt("Enter your experience");
+//     if (exp === "2 years") {
+//       console.log("Congratulation, you are Hired");
+//     } else {
+//       console.log("Barhe ho jao");
+//     }
+//   } else {
+//     console.log("Karachi Aaja bhai");
+//   }
+// } else {
+//   console.log("JavaScript Seekho Beta");
+// }
+
+//  Falsy Value (6 types) other than this values are true
+
+// "" Empty String ==> False
+// 0 ==> False
+// false ==> False
+// undefind ==> False
+//NaN ==> False
+// null ==> False
