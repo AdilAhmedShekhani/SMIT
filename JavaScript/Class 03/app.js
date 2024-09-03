e0525cf56701e7248f497f95cd6a1b23bc18e7f9
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-09-03 09:03:59 +0500

    Comparison Operators

--- JavaScript/Class 03/app.js
+++ JavaScript/Class 03/app.js
@@ -1,1 +1,30 @@
 alert("Hello Students");
+
+//Comparison operators: ==, ===,!=, !==, etc.:
+//Comparison operators are used to compare values in JavaScript. JavaScript supports various comparison operators, including the equal (==) operator, strict equal (===) operator, not equal (!=) operator, and strict not equal (!==) operator.
+
+// // Equal operator
+// var x = 5
+// if (x == 5) {
+//   console.log("x is equal to 5")
+// }
+
+// // Strict equal operator
+// var y = "5"
+// if (y === 5) {
+//   console.log("y is equal to 5")
+// } else {
+//   console.log("y is not equal to 5")
+// }
+
+// // Not equal operator
+// var z = 10
+// if (z != 5) {
+//   console.log("z is not equal to 5")
+// }
+
+// // Strict not equal operator
+// var a = "5"
+// if (a !== 5) {
+//   console.log("a is not equal to 5")
+// }
